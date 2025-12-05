c9ba854eecf32535f601bc26f46939f175566563
emilianoym <emiliano.yepez30@gmail.com>
2025-12-04 23:04:34 -0600

    Fix swagger config

--- swagger.js
+++ swagger.js
@@ -1,30 +1,30 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-// Swagger configuration
-const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'Documentación de la API',
-    version: '1.0.0',
-    description: 'Documentación de la API con Swagger',
-  },
-  servers: [
-    {
-      url: '/',
-      description: 'Servidor de desarrollo',
-    },
-  ],
-};
-
 const options = {
-  definition: swaggerDefinition,
-  apis: ['./routes/*.js'],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Documentación de la API",
+      version: "1.0.0",
+      description: "Documentación con Swagger",
+    },
+    servers: [
+      {
+        url: "https://proyectofinalserviciosweb-production.up.railway.app",
+        description: "Producción",
+      },
+      {
+        url: "http://localhost:4000",
+        description: "Local",
+      }
+    ],
+  },
+  apis: ["./routes/*.js"],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
-// Setup Swagger UI
 function setupSwagger(app) {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
